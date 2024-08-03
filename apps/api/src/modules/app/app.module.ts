cc6b597272fe8adc1d464127d61aef39e61b6e68
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-03 18:56:20 +0300

    Create Database service in API app

--- apps/api/src/modules/app/app.module.ts
+++ apps/api/src/modules/app/app.module.ts
@@ -2,7 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from '~/modules/app/app.controller';
 
+import { EmployeesModule } from '~/modules/employees/employees.module';
+
 @Module({
+  imports: [EmployeesModule],
   controllers: [AppController]
 })
 export class AppModule {}
