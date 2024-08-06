3277b845d0e37f158d0394f83152ddc360cd2867
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-07 01:41:39 +0300

    Remove UI & Tailwind config packages

--- apps/employee-management/src/main.tsx
+++ apps/employee-management/src/main.tsx
@@ -1,17 +1,13 @@
-import '@repo/ui/styles.css';
 import './globals.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import { Button } from '@repo/ui/button';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <div>
       <h1>Hello, World!</h1>
-      <Button />
     </div>
   </React.StrictMode>
 );
