fee2f20a5f872c5935b3195a121a540a00b63c12
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-08 00:13:10 +0300

    Enable CORS in the API app

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from '~/modules/app/app.module';
 const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(3000);
