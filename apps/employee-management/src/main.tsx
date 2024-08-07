717333de03d768b209dc136b594283c0a6e2d7d7
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-07 22:26:27 +0300

    Update TailwindCSS settings for shadcn/ui component library

--- apps/employee-management/src/main.tsx
+++ apps/employee-management/src/main.tsx
@@ -6,8 +6,8 @@ import ReactDOM from 'react-dom/client';
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <div>
-      <h1>Hello, World!</h1>
+    <div className='min-h-screen bg-background font-sans antialiased'>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </div>
   </React.StrictMode>
 );
