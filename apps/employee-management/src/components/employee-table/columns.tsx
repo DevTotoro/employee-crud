3878e7cc80874c186a067983df8fc20dff12edfe
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-08 00:50:31 +0300

    Create table for employees in Employee management app

--- apps/employee-management/src/components/employee-table/columns.tsx
+++ apps/employee-management/src/components/employee-table/columns.tsx
@@ -1,5 +1,16 @@
 import { ColumnDef } from '@tanstack/react-table';
 import type { Employee } from '@repo/database';
+import { MoreHorizontal, ArrowUpDown } from 'lucide-react';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '~/components/ui/dropdown-menu';
+import { Button } from '~/components/ui/button';
 
 export const columns: ColumnDef<Employee>[] = [
   {
@@ -8,11 +19,21 @@ export const columns: ColumnDef<Employee>[] = [
   },
   {
     accessorKey: 'lastName',
-    header: 'Last Name'
+    header: ({ column }) => (
+      <Button
+        variant='ghost'
+        onClick={() => {
+          column.toggleSorting(column.getIsSorted() === 'asc');
+        }}
+      >
+        Last name
+        <ArrowUpDown className='ml-2 h-4 w-4' />
+      </Button>
+    )
   },
   {
     accessorKey: 'firstName',
-    header: 'First Name'
+    header: 'First name'
   },
   {
     accessorKey: 'email',
@@ -20,10 +41,39 @@ export const columns: ColumnDef<Employee>[] = [
   },
   {
     accessorKey: 'createdAt',
-    header: 'Creation Date'
+    header: 'Creation Date',
+    cell: ({ row }) => new Date(row.original.createdAt).toLocaleDateString()
   },
   {
     accessorKey: 'updatedAt',
-    header: 'Last Updated'
+    header: 'Last Updated',
+    cell: ({ row }) => new Date(row.original.updatedAt).toLocaleDateString()
+  },
+  {
+    id: 'actions',
+    cell: ({ row }) => {
+      const employee = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='ghost' className='h-8 w-8 p-0'>
+              <span className='sr-only'>Open menu</span>
+              <MoreHorizontal className='h-4 w-4' />
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align='end'>
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem onClick={() => void navigator.clipboard.writeText(employee.id)}>
+              Copy employee ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Edit employee</DropdownMenuItem>
+            <DropdownMenuItem>Delete employee</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    }
   }
 ];
--- apps/employee-management/src/pages/employees/index.tsx
+++ apps/employee-management/src/pages/employees/index.tsx
@@ -1,5 +1,16 @@
+import type { Employee } from '@repo/database';
+
+import { useApi } from '~/lib/hooks/use-api';
 import { EmployeeTable } from '~/components/employee-table';
 
 export const EmployeesPage = () => {
-  return <EmployeeTable />;
+  const { data } = useApi<Employee[]>({
+    endpoint: '/employees',
+    method: 'GET',
+    errorMessages: {
+      500: 'An error occured while fetching employees'
+    }
+  });
+
+  return <EmployeeTable employees={data} />;
 };
