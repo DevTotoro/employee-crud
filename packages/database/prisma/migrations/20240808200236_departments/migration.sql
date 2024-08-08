503d7fef32ccb07e50e133f5ea72a007e4265ff9
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-08 23:03:50 +0300

    Create database table "departments"

---
+++ packages/database/prisma/migrations/20240808200236_departments/migration.sql
@@ -0,0 +1,18 @@
+-- AlterTable
+ALTER TABLE "employees" ADD COLUMN     "department_id" TEXT;
+
+-- CreateTable
+CREATE TABLE "departments" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "departments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "departments_name_key" ON "departments"("name");
+
+-- AddForeignKey
+ALTER TABLE "employees" ADD CONSTRAINT "employees_department_id_fkey" FOREIGN KEY ("department_id") REFERENCES "departments"("id") ON DELETE SET NULL ON UPDATE CASCADE;
