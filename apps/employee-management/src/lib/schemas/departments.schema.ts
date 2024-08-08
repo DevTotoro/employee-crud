f0e69cd358b7e238a566347fffe99e56c22ef085
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-09 00:43:16 +0300

    Implement department editing & deleting in Employee management app

--- apps/employee-management/src/lib/schemas/departments.schema.ts
+++ apps/employee-management/src/lib/schemas/departments.schema.ts
@@ -4,3 +4,8 @@ export type CreateDepartmentSchema = z.infer<typeof createDepartmentSchema>;
 export const createDepartmentSchema = z.object({
   name: z.string().min(2).max(50)
 });
+
+export type EditDepartmentSchema = z.infer<typeof editDepartmentSchema>;
+export const editDepartmentSchema = z.object({
+  name: createDepartmentSchema.shape.name
+});
