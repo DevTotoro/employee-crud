a51d886e4039d0aaef23758e7f9189f9bad27381
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-09 01:22:30 +0300

    Implement department picking functionality for employees in Employee management app

--- apps/employee-management/src/lib/schemas/employees.schema.ts
+++ apps/employee-management/src/lib/schemas/employees.schema.ts
@@ -4,11 +4,13 @@ export type CreateEmployeeSchema = z.infer<typeof createEmployeeSchema>;
 export const createEmployeeSchema = z.object({
   email: z.string().email(),
   firstName: z.string().min(2).max(50),
-  lastName: z.string().min(2).max(50)
+  lastName: z.string().min(2).max(50),
+  departmentId: z.string().optional()
 });
 
 export type EditEmployeeSchema = z.infer<typeof editEmployeeSchema>;
 export const editEmployeeSchema = z.object({
   firstName: createEmployeeSchema.shape.firstName,
-  lastName: createEmployeeSchema.shape.lastName
+  lastName: createEmployeeSchema.shape.lastName,
+  departmentId: createEmployeeSchema.shape.departmentId
 });
