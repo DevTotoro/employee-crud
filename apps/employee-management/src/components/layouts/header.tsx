1d0385fecaa63ce6566d8260bc4d0520c5fe251e
DevTotoro <totoro.softwaredev@gmail.com>
2024-08-08 23:13:33 +0300

    Create departments page & add links to header in Employee management app

--- apps/employee-management/src/components/layouts/header.tsx
+++ apps/employee-management/src/components/layouts/header.tsx
@@ -1,17 +1,29 @@
 import { Link } from 'react-router-dom';
 import { BriefcaseBusiness } from 'lucide-react';
 
+import { Button } from '~/components/ui/button';
+
 export const Header = () => {
   return (
     <header className='sticky top-0 z-50 w-full border-b bg-background'>
       <div className='container flex h-16 items-center justify-between'>
-        <Link to='/employees' className='flex items-center space-x-2'>
+        <Link to='/employees' className='flex items-center gap-2'>
           <BriefcaseBusiness className='size-6' aria-hidden='true' />
 
           <span className='hidden font-bold sm:inline-block'>Employee management</span>
 
           <span className='sr-only'>Home</span>
         </Link>
+
+        <div className='flex items-center gap-2'>
+          <Button variant='link' asChild>
+            <Link to='/employees'>Employees</Link>
+          </Button>
+
+          <Button variant='link' asChild>
+            <Link to='/departments'>Departments</Link>
+          </Button>
+        </div>
       </div>
     </header>
   );
--- apps/employee-management/src/lib/routes.tsx
+++ apps/employee-management/src/lib/routes.tsx
@@ -1,7 +1,8 @@
 import { RouteObject, Navigate } from 'react-router-dom';
-import { BaseLayout } from '~/components/layouts/base';
 
+import { BaseLayout } from '~/components/layouts/base';
 import { EmployeesPage } from '~/pages/employees';
+import { DepartmentsPage } from '~/pages/departments';
 
 export const routes: RouteObject[] = [
   {
@@ -14,6 +15,10 @@ export const routes: RouteObject[] = [
       {
         path: '/employees',
         element: <EmployeesPage />
+      },
+      {
+        path: '/departments',
+        element: <DepartmentsPage />
       }
     ]
   }
